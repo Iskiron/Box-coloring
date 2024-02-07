926cce002a5afeb2d37392104b2b205082ac3e92
Iskiron <139690281+Iskiron@users.noreply.github.com>
2024-02-07 17:38:42 +0530

    file

---
+++ script.js
@@ -0,0 +1,20 @@
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  
+  
+
+  document.getElementById("btn").onclick = () => {
+    const boxes = document.querySelectorAll('.box');
+  
+  boxes.forEach(e => {
+    e.style.backgroundColor = getRandomColor();
+  });
+  }
+  
